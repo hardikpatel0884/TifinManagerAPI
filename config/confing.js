9f7a2f51b138b16681ebd782658f204a2de8b526
Hardik Patel <hardikpatel0884@gmail.com>
2018-03-27 23:22:55 +0530

    add comment

--- config/confing.js
+++ config/confing.js
@@ -4,11 +4,14 @@
  * */
 
 module.exports = {
+    /** port number to run application */
     port: process.env.PORT || 3000,
+
+    /** mysql connection properties */
     mysql:{
         hostname:"localhost",
         database:"tifinmanager",
         username:"root",
         password:"Har884dik"
     }
-}
+};
--- server.js
+++ server.js
@@ -3,11 +3,19 @@
  * main server file
  * */
 
+/**
+ * require modules
+ * express: to run application
+ * config: basic configuration of application
+ * bodyparser: parse request body
+ * mysql: connection with mysql database
+ * */
 const express = require("express"),
     config = require("./config/confing"),
     bodyparser = require("body-parser"),
     mysql = require("mysql"),
     app = express(),
+    /** create connection properties */
     connection = mysql.createConnection({
         host: config.mysql.hostname,
         user: config.mysql.username,
@@ -15,9 +23,10 @@ const express = require("express"),
         database: config.mysql.database
     });
 
-app.use(bodyparser())
+app.use(bodyparser());
+/** connect with database */
 connection.connect();
-
+/** set application port */
 app.listen(config.port, () => {
     console.log(`magic start on port ${config.port}`);
 });
